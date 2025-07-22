d6a84167401cd3140cf47d7fb33e9c76e88e2e22
dave tush <mustaphadavid98@gmail.com>
2025-07-22 23:18:45 +0100

    selection_screen.dart

--- lib/screens/selection_screen.dart
+++ lib/screens/selection_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:foodie_padi_apps/screens/homepage_screen';
+
+import 'homepage_screen.dart';
 
 class ChooseRoleScreen extends StatelessWidget {
   const ChooseRoleScreen({super.key});
