5835dbe8b7b06972292b019ce586696d479f1152
dave tush <mustaphadavid98@gmail.com>
2025-07-24 23:56:41 +0100

    password screen

--- lib/screens/create_new_password_screen.dart
+++ lib/screens/create_new_password_screen.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:provider/provider.dart';
 
-class CreateNewPassword extends StatelessWidget {
-  const CreateNewPassword({Key? key}) : super(key: key);
+import '../constants/app_colors.dart';
+import '../providers/signup_provider.dart';
+import '../widgets/passwordfield.dart';
+
+class CreateNewPasswordScreen extends StatelessWidget {
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController confirmPasswordController =
+      TextEditingController();
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
+    final provider = Provider.of<SignUpProvider>(context);
+
     return Scaffold(
       backgroundColor: AppColors.primaryOrange,
       body: Padding(
@@ -15,17 +26,28 @@ class CreateNewPassword extends StatelessWidget {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text("Create New Password", style: TextStyle(fontSize: 24.sp, color: Colors.white, fontWeight: FontWeight.bold)),
+                Text("Create New Password",
+                    style: TextStyle(
+                        fontSize: 24.sp,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold)),
                 SizedBox(height: 20.h),
-                buildPasswordField("New Password", passwordController, provider.obscurePassword,
+                buildPasswordField(
+                    "New Password",
+                    passwordController,
+                    provider.obscurePassword,
                     provider.togglePasswordVisibility),
-                buildPasswordField("Confirm Password", confirmPasswordController, provider.obscureConfirmPassword,
+                buildPasswordField(
+                    "Confirm Password",
+                    confirmPasswordController,
+                    provider.obscureConfirmPassword,
                     provider.toggleConfirmPasswordVisibility),
                 SizedBox(height: 20.h),
                 ElevatedButton(
                   onPressed: () {
                     if (_formKey.currentState!.validate()) {
-                      if (passwordController.text != confirmPasswordController.text) {
+                      if (passwordController.text !=
+                          confirmPasswordController.text) {
                         ScaffoldMessenger.of(context).showSnackBar(SnackBar(
                           content: Text("Passwords do not match!"),
                           backgroundColor: Colors.red,
